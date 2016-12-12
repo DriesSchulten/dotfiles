a5e9fcfeeb272d8d0471cf8bcc241d0f6afff902
Dries Schulten <dries.schulten@fastmail.com>
2016-12-12 21:09:23 +0100

    ZPlug update.

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -23,9 +23,6 @@ if [[ $OSTYPE == *darwin* ]]; then
   # GO
   export PATH=$PATH:/usr/local/opt/go/libexec/bin
 
-  # MAMP
-  export PATH=/Applications/MAMP/bin/php/php5.6.10/bin:$PATH
-
   # Pad
   export PATH=/usr/local/bin:/usr/local/sbin:/usr/local/share/npm/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$GEM_HOME/bin:$PATH
 else
--- zsh/zplug-setup.zsh
+++ zsh/zplug-setup.zsh
@@ -12,23 +12,23 @@ source ~/.zplug/init.zsh
 zplug "zplug/zplug"
 
 # Plugins
-zplug "zsh-users/zsh-syntax-highlighting", nice:10
+zplug "zsh-users/zsh-syntax-highlighting", defer:2
 zplug "zsh-users/zsh-history-substring-search"
 zplug "zsh-users/zsh-completions"
 zplug "peterhurford/git-it-on.zsh"
 zplug "mafredri/zsh-async"
 zplug "robbyrussell/oh-my-zsh", use:"lib/key-bindings.zsh"
 zplug "robbyrussell/oh-my-zsh", use:"lib/completion.zsh"
-zplug "plugins/sudo", from:oh-my-zsh
-zplug "plugins/gradle", from:oh-my-zsh, nice:10
-zplug "plugins/docker", from:oh-my-zsh
-zplug "plugins/brew", from:oh-my-zsh, if:$OSX
-zplug "plugins/brew-cask", from:oh-my-zsh, if:$OSX, nice:10
-zplug "plugins/osx", from:oh-my-zsh, if:$OSX, nice:10
+zplug "plugins/sudo", from:oh-my-zsh, as:plugin
+zplug "plugins/gradle", from:oh-my-zsh, defer:2, as:plugin
+zplug "plugins/docker", from:oh-my-zsh, as:plugin
+zplug "plugins/brew", from:oh-my-zsh, if:$OSX, as:plugin
+zplug "plugins/brew-cask", from:oh-my-zsh, if:$OSX, defer:2, as:plugin
+zplug "plugins/osx", from:oh-my-zsh, if:$OSX, defer:2, as:plugin
 zplug "b4b4r07/enhancd", use:init.sh
 
 # Local plugins
-zplug "~/.zsh/conf", from:local, nice:12, if:$OSX
+zplug "~/.zsh/conf", from:local, defer:3, if:$OSX
 
 # Theme
 zplug "sindresorhus/pure"

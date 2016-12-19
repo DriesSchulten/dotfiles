84651ab0fdef8665f0a379e5aab0fd118b652ad1
Dries Schulten <dries.schulten@fastmail.com>
2016-12-19 10:52:10 +0100

    Clean up.

--- zsh/conf/java.zsh
+++ zsh/conf/java.zsh
@@ -1,15 +1,5 @@
 if which jenv > /dev/null; then eval "$(jenv init -)"; fi
 
-# Set Java7/Java8 home (retrolambda)
-export JAVA7_HOME=${HOME}/.jenv/versions/1.7
-export JAVA8_HOME=${HOME}/.jenv/versions/1.8
-
-# Maven
-export MAVEN_OPTS="-Xmx1024m -Dfile.encoding=UTF-8 -Djava.awt.headless=true"
-
-# Ant
-export ANT_OPTS="-Xmx1024m"
-
 # Android
 export ANDROID_HOME=/usr/local/opt/android-sdk
 

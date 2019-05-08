c9b8fd4bedcfc0e51c3e5802e0ff0b26599401fc
Dries Schulten <dries.schulten@fastmail.com>
2019-05-08 14:48:03 +0200

    Add autojump.

--- fish/config.fish
+++ fish/config.fish
@@ -2,5 +2,8 @@ set --export SHELL /usr/bin/fish
 set --export EDITOR "subl -w"
 set -e fish_greeting
 
+[ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
+[ -f ~/Library/autojump/autojump.txt ]; and alias j="cd (cat ~/Library/autojump/autojump.txt | sort -nr | awk '{print \$2}' | fzf +s)"
+
 # Android
 export ANDROID_HOME=/usr/local/share/android-sdk

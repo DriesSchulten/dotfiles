184939b2b92ce5ad6b78200ac2fb576ecae0afa1
Dries Schulten <dries@schulten.me>
2023-07-03 11:22:50 +0200

    Change to OrbStack.

--- fish/config.fish
+++ fish/config.fish
@@ -6,6 +6,7 @@ set -U fish_greeting
 set -g fish_user_paths "/opt/homebrew/sbin" $fish_user_paths
 set -g fish_user_paths "/opt/homebrew/bin" $fish_user_paths
 set -g fish_user_paths (ruby -e 'print Gem.user_dir')/bin $fish_user_paths
+set -g fish_user_paths "/Users/dries/.cargo/bin" $fish_user_paths
 
 starship init fish | source
 
@@ -13,7 +14,7 @@ export NVM_DIR="$HOME/.nvm"
 export NVM_SYMLINK_CURRENT=true
  
 [ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
-[ -f ~/Library/autojump/autojump.txt ]; and alias j="cd (cat ~/Library/autojump/autojump.txt | sort -nr | awk '{print \$2}' | fzf +s)"
+[ -f ~/Library/autojump/autojump.txt ]; and alias j="cd (cat ~/Library/autojump/autojump.txt | sort -nr | awk -F '\t' '{print \$NF}' | fzf +s)"
 
 # Android
 export ANDROID_HOME=/opt/homebrew/share/android-sdk

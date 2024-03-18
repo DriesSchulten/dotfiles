2fc209cbd1eb23b8a2b6f3b2fdc5a4006b8859af
Dries Schulten <dries@schulten.me>
2024-03-18 12:03:31 +0100

    Remove macOS check.

--- install.sh
+++ install.sh
@@ -5,20 +5,17 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Update dotfiles itself first
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
-# Install stuff when on OS X
-if [[ $OSTYPE == *darwin* ]]; then
-  # Install homebrew
-  . "$DOTFILES_DIR/homebrew.sh"
+# Install homebrew
+. "$DOTFILES_DIR/homebrew.sh"
 
-  # Install FISH
-  . "$DOTFILES_DIR/fish.mac.sh"
+# Install FISH
+. "$DOTFILES_DIR/fish.sh"
 
-  # Sublime merge
-  . "$DOTFILES_DIR/smerge.sh"
+# Sublime merge
+. "$DOTFILES_DIR/smerge.sh"
 
-  # SDKMan
-  . "$DOTFILES_DIR/sdkman.sh"
-fi
+# SDKMan
+. "$DOTFILES_DIR/sdkman.sh"
 
 # Link
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
@@ -34,8 +31,3 @@ ln -sfv "$DOTFILES_DIR/fish/fish_plugins" ~/.config/fish/
 ln -sfv "$DOTFILES_DIR/starship/starship.toml" ~/.config/
 
 fish "$DOTFILES_DIR/fish/fisher-setup.fish"
-
-if [[ $OSTYPE == *darwin* ]]; then
-  # Launchctl vars
-  launchctl setenv ANDROID_HOME /opt/homebrew/share/android-sdk/
-fi

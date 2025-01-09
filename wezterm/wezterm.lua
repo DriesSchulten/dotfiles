9e7007e3862770d1b7eb4fc568a3afe6b6dbb709
Dries Schulten <dries@schulten.me>
2025-01-09 11:34:53 +0100

    Remote iTerm config

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local balance = require("balance")
 local act = wezterm.action
 local config = wezterm.config_builder()
 local mux = wezterm.mux
@@ -8,92 +9,100 @@ local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.
 
 config.enable_tab_bar = true
 config.window_background_opacity = 0.9
-config.macos_window_background_blur = 10
+config.macos_window_background_blur = 15
 config.native_macos_fullscreen_mode = true
 config.font_size = 11.0
 config.color_scheme = "Catppuccin Mocha"
 
 resurrect.periodic_save({
-    interval_seconds = 300,
-    save_tabs = true,
-    save_windows = true,
-    save_workspaces = true,
+	interval_seconds = 300,
+	save_tabs = true,
+	save_windows = true,
+	save_workspaces = true,
 })
 
 tabline.setup({
-    options = {
-        icons_enabled = true,
-        theme = "Catppuccin Mocha",
-        color_overrides = {},
-        section_separators = {
-            left = wezterm.nerdfonts.ple_right_half_circle_thick,
-            right = wezterm.nerdfonts.ple_left_half_circle_thick,
-        },
-        component_separators = {
-            left = wezterm.nerdfonts.ple_right_half_circle_thin,
-            right = wezterm.nerdfonts.ple_left_half_circle_thin,
-        },
-        tab_separators = {
-            left = wezterm.nerdfonts.ple_right_half_circle_thick,
-            right = wezterm.nerdfonts.ple_left_half_circle_thick,
-        },
-    },
-    sections = {
-        tabline_a = { "mode" },
-        tabline_b = { "workspace" },
-        tabline_c = { " " },
-        tab_active = {
-            "index",
-            { "parent", padding = 0 },
-            "/",
-            { "cwd",    max_length = 30, padding = { left = 0, right = 1 } },
-            { "zoomed", padding = 0 },
-        },
-        tab_inactive = { "index", { "process", padding = { left = 0, right = 1 } } },
-        tabline_x = { "ram", "cpu" },
-        tabline_y = { "battery" },
-        tabline_z = { { "datetime", style = "%Y-%m-%d %H:%M:%S" } },
-    },
-    extensions = {},
+	options = {
+		icons_enabled = true,
+		theme = "Catppuccin Mocha",
+		color_overrides = {},
+		section_separators = {
+			left = wezterm.nerdfonts.ple_right_half_circle_thick,
+			right = wezterm.nerdfonts.ple_left_half_circle_thick,
+		},
+		component_separators = {
+			left = wezterm.nerdfonts.ple_right_half_circle_thin,
+			right = wezterm.nerdfonts.ple_left_half_circle_thin,
+		},
+		tab_separators = {
+			left = wezterm.nerdfonts.ple_right_half_circle_thick,
+			right = wezterm.nerdfonts.ple_left_half_circle_thick,
+		},
+	},
+	sections = {
+		tabline_a = { "mode" },
+		tabline_b = { "workspace" },
+		tabline_c = { " " },
+		tab_active = {
+			"index",
+			{ "parent", padding = 0 },
+			"/",
+			{ "cwd", max_length = 30, padding = { left = 0, right = 1 } },
+			{ "zoomed", padding = 0 },
+		},
+		tab_inactive = { "index", { "process", padding = { left = 0, right = 1 } } },
+		tabline_x = { "ram", "cpu" },
+		tabline_y = { "battery" },
+		tabline_z = { { "datetime", style = "%Y-%m-%d %H:%M:%S" } },
+	},
+	extensions = {},
 })
 tabline.apply_to_config(config)
 
 wezterm.on("gui-startup", function(cmd)
-    local _, pane, window = mux.spawn_window(cmd or {})
-    local gui_window = window:gui_window()
-    gui_window:perform_action(wezterm.action.ToggleFullScreen, pane)
-    pane:split({})
-    pane:activate()
+	local _, pane, window = mux.spawn_window(cmd or {})
+	local gui_window = window:gui_window()
+	gui_window:perform_action(wezterm.action.ToggleFullScreen, pane)
+	pane:split({})
+	pane:activate()
 end)
 
 config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
 config.keys = {
-    { key = "a", mods = "LEADER|CTRL",  action = act.SendKey({ key = "a", mods = "CTRL" }) },
+	{ key = "a", mods = "LEADER|CTRL", action = act.SendKey({ key = "a", mods = "CTRL" }) },
 
-    { key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-    { key = "-", mods = "LEADER",       action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
-    { key = "z", mods = "LEADER",       action = "TogglePaneZoomState" },
-    { key = "c", mods = "LEADER",       action = act({ SpawnTab = "CurrentPaneDomain" }) },
+	{ key = "|", mods = "LEADER|SHIFT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "-", mods = "LEADER", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "z", mods = "LEADER", action = "TogglePaneZoomState" },
+	{ key = "c", mods = "LEADER", action = act({ SpawnTab = "CurrentPaneDomain" }) },
 
-    { key = "h", mods = "LEADER",       action = act.ActivatePaneDirection("Left") },
-    { key = "j", mods = "LEADER",       action = act.ActivatePaneDirection("Down") },
-    { key = "k", mods = "LEADER",       action = act.ActivatePaneDirection("Up") },
-    { key = "l", mods = "LEADER",       action = act.ActivatePaneDirection("Right") },
+	{ key = "h", mods = "LEADER", action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER", action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER", action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER", action = act.ActivatePaneDirection("Right") },
 
-    { key = "`", mods = "LEADER",       action = act.ActivateLastTab },
-    { key = "n", mods = "LEADER",       action = act.ActivateTabRelative(1) },
-    { key = "p", mods = "LEADER",       action = act.ActivateTabRelative(-1) },
-    { key = "1", mods = "LEADER",       action = act({ ActivateTab = 0 }) },
-    { key = "2", mods = "LEADER",       action = act({ ActivateTab = 1 }) },
-    { key = "3", mods = "LEADER",       action = act({ ActivateTab = 2 }) },
-    { key = "4", mods = "LEADER",       action = act({ ActivateTab = 3 }) },
-    { key = "5", mods = "LEADER",       action = act({ ActivateTab = 4 }) },
-    { key = "6", mods = "LEADER",       action = act({ ActivateTab = 5 }) },
-    { key = "7", mods = "LEADER",       action = act({ ActivateTab = 6 }) },
-    { key = "8", mods = "LEADER",       action = act({ ActivateTab = 7 }) },
-    { key = "9", mods = "LEADER",       action = act({ ActivateTab = 8 }) },
+	{ key = "`", mods = "LEADER", action = act.ActivateLastTab },
+	{ key = "n", mods = "LEADER", action = act.ActivateTabRelative(1) },
+	{ key = "p", mods = "LEADER", action = act.ActivateTabRelative(-1) },
+	{ key = "1", mods = "LEADER", action = act({ ActivateTab = 0 }) },
+	{ key = "2", mods = "LEADER", action = act({ ActivateTab = 1 }) },
+	{ key = "3", mods = "LEADER", action = act({ ActivateTab = 2 }) },
+	{ key = "4", mods = "LEADER", action = act({ ActivateTab = 3 }) },
+	{ key = "5", mods = "LEADER", action = act({ ActivateTab = 4 }) },
+	{ key = "6", mods = "LEADER", action = act({ ActivateTab = 5 }) },
+	{ key = "7", mods = "LEADER", action = act({ ActivateTab = 6 }) },
+	{ key = "8", mods = "LEADER", action = act({ ActivateTab = 7 }) },
+	{ key = "9", mods = "LEADER", action = act({ ActivateTab = 8 }) },
 
-    { key = "&", mods = "LEADER|SHIFT", action = act({ CloseCurrentPane = { confirm = true } }) },
+	{ key = "&", mods = "LEADER|SHIFT", action = act({ CloseCurrentPane = { confirm = true } }) },
+	{
+		key = "b",
+		mods = "LEADER",
+		action = act.Multiple({
+			wezterm.action_callback(balance.balance_panes("x")),
+			wezterm.action_callback(balance.balance_panes("y")),
+		}),
+	},
 }
 
 return config

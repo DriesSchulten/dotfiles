4cd589b46262a152b6020a02fa5593b5a0fa4e2a
Dries Schulten <dries@schulten.me>
2025-01-24 09:51:00 +0100

    Update Linux part

--- .chezmoihooks/install-password-manager.sh
+++ .chezmoihooks/install-password-manager.sh
@@ -7,9 +7,6 @@ if command -v op >/dev/null 2>&1; then
     exit
 fi
 
-tmpdir=$(mktemp -d -t install-package-manager.XXXXXX)
-trap 'rm -rf "$tmpdir"' EXIT INT TERM
-
 case "$(uname -s)" in
 Darwin)
     if ! [ -x "$HOMEBREW_PREFIX/bin/brew" ]; then
@@ -36,6 +33,8 @@ Linux)
         exit 1
         ;;
     esac
+    tmpdir=$(mktemp -d -t install-package-manager.XXXXXX)
+    trap 'rm -rf "$tmpdir"' EXIT INT TERM
     OP_VERSION="v$(curl https://app-updates.agilebits.com/check/1/0/CLI2/en/2.0.0/N -s | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')";
     wget "https://cache.agilebits.com/dist/1P/op2/pkg/${OP_VERSION}/op_linux_${ARCH}_${OP_VERSION}.zip" -O "$tmpdir/op.zip" &&
         unzip -d ~/.local/bin "$tmpdir/op.zip" op

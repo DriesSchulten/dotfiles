d8cc334e84691e5b04384668f6b882d0880df386
Dries Schulten <dries@schulten.me>
2025-01-29 10:48:25 +0100

    Add transparent bg again.

--- dot_config/nvim/lua/plugins/colortheme.lua
+++ dot_config/nvim/lua/plugins/colortheme.lua
@@ -1,8 +1,12 @@
 return {
-  'catppuccin/nvim',
-  lazy = false,
-  priority = 1000,
-  config = function()
-    vim.cmd.colorscheme 'catppuccin-mocha'
-  end,
+	"catppuccin/nvim",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			transparent_background = true,
+		})
+		vim.cmd.colorscheme("catppuccin-mocha")
+	end,
 }
+-- vim: ts=2 sts=2 sw=2 et
